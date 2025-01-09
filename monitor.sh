8d741b00cfe5c738a555e3db1ce0d5e8d849df96
Ziggy <siegfriedporterfield@gmail.com>
2025-01-09 17:09:03 +0100

    echo pwd in monitor

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,5 @@
 df -H |  stdbuf -o0 tee -a /dev/stdout
+echo "PWD:" $(pwd) |  stdbuf -o0 tee -a /dev/stdout
 while true; do
   echo "CPU Usage:" $((100-$(vmstat 1 2|tail -1|awk '{print $15}')))"%" |  stdbuf -o0 tee -a /dev/stdout
   echo "Memory Usage: $(free | grep Mem | awk '{print $3/$2 * 100.0 "%"}')" |  stdbuf -o0 tee -a /dev/stdout
