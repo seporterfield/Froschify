9cb09450571a0417ff82e7770f456606c2fe260b
Ziggy <siegfriedporterfield@gmail.com>
2025-01-09 19:20:55 +0100

    monitor: remove redundant log at /dev/stdout

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,5 @@
-echo "Storage:" $(df -H $(pwd) | tail -n 1) |  stdbuf -o0 tee -a /dev/stdout
+echo "Storage:" $(df -H $(pwd) | tail -n 1) |  stdbuf -o0 cat >> /dev/stdout
 while true; do
-  echo "CPU Usage:" $((100-$(vmstat 1 2|tail -1|awk '{print $15}')))"%" |  stdbuf -o0 tee -a /dev/stdout
-  echo "Memory Usage: $(free | grep Mem | awk '{print $3/$2 * 100.0 "%"}')" |  stdbuf -o0 tee -a /dev/stdout
+  echo "CPU:" $((100-$(vmstat 1 2|tail -1|awk '{print $15}')))"%," "Mem: $(free | grep Mem | awk '{print $3/$2 * 100.0 "%"}')" |  stdbuf -o0 cat >> /dev/stdout
   sleep 5
 done
