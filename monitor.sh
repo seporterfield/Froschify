ba4e343e89c67bcbc390b62e2b2d0d03b01374bb
Ziggy <siegfriedporterfield@gmail.com>
2025-01-09 17:14:45 +0100

    look at fs usage where app is stored in monitor

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,4 @@
-df -H |  stdbuf -o0 tee -a /dev/stdout
-echo "PWD:" $(pwd) |  stdbuf -o0 tee -a /dev/stdout
+echo "Storage:" $(df -H $(pwd) | tail -n 1) |  stdbuf -o0 tee -a /dev/stdout
 while true; do
   echo "CPU Usage:" $((100-$(vmstat 1 2|tail -1|awk '{print $15}')))"%" |  stdbuf -o0 tee -a /dev/stdout
   echo "Memory Usage: $(free | grep Mem | awk '{print $3/$2 * 100.0 "%"}')" |  stdbuf -o0 tee -a /dev/stdout
