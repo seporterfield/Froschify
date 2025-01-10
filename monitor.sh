c679ec5ecaaf9620dace9433e03def306d339ac4
Ziggy <siegfriedporterfield@gmail.com>
2025-01-10 12:31:45 +0100

    clean up monitor script and add uptime counter

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,10 @@
-echo "Storage:" $(df -H $(pwd) | tail -n 1) |  stdbuf -o0 cat >> /dev/stdout
+starttime=$(date +%s)
+echo 'Output of df -H $(pwd):' $(df -H $(pwd) | tail -n 1) | stdbuf -o0 cat >>/dev/stdout
 while true; do
-  echo "CPU:" $((100-$(vmstat 1 2|tail -1|awk '{print $15}')))"%," "Mem: $(free | grep Mem | awk '{print $3/$2 * 100.0 "%"}')" |  stdbuf -o0 cat >> /dev/stdout
+  elapsed=$(($(date +%s) - $starttime))
+  memusg=$(free | grep Mem | awk '{print $3/$2 * 100.0 "%"}' | sed 's/%//' | cut -d"." -f1)
+  cpuusg=$((100 - $(vmstat 1 2 | tail -1 | awk '{print $15}')))
+  uptime=$(printf "%02d:%02d:%02d" $(($elapsed / 3600)) $(($elapsed / 60 % 60)) $(($elapsed % 60)))
+  printf "Uptime %s CPU%% %02d Mem%% %02d\n" $uptime $cpuusg $memusg | stdbuf -o0 cat >>/dev/stdout
   sleep 5
 done
