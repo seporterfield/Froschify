236d1d8a9ade009ff7b190c3fc4052c7e2f3647c
Siegfried <siegfriedporterfield@gmail.com>
2025-05-11 15:01:45 +0200

    try adding git sha

--- src/main.py
+++ src/main.py
@@ -62,7 +62,9 @@ def create_app() -> FastAPI:
 
     @app.get("/", response_class=HTMLResponse)
     async def home(request: Request) -> Response:
-        return templates.TemplateResponse(request, "index.html")
+        return templates.TemplateResponse(
+            request, "index.html", context={"git_sha": "blabla123"}
+        )
 
     @app.get("/healthz", status_code=status.HTTP_200_OK)
     async def health(request: Request) -> PlainTextResponse:
